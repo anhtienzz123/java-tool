3e493860a95c8bba6b2826bf0b8f34949d4a347f
hatien <hatien@tma.com.vn>
2023-05-09 16:06:41 +0700

    init selenium

---
+++ src/main/java/selenium/SeleniumConfig.java
@@ -0,0 +1,8 @@
+package selenium;
+
+public class SeleniumConfig {
+
+    public static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
+    
+    public static final String CHROME_DRIVER_PATH = "D:/app/selenium/chromedriver.exe";
+}
